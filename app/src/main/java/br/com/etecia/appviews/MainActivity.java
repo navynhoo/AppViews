b55e91f8bc7807322942e2b8183fa9df4f1bcb20
manager <pedro.cruz44@etec.sp.gov.br>
2019-08-28 21:48:10 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/appviews/MainActivity.java
@@ -0,0 +1,46 @@
+package br.com.etecia.appviews;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText txtNum1, txtNum2;
+
+    Button btnSomar;
+
+    TextView lblResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtNum1 = (EditText) findViewById(R.id.txtNum1);
+        txtNum2 = (EditText) findViewById(R.id.txtNum2);
+
+        lblResultado = (TextView) findViewById(R.id.lblResultado);
+
+        btnSomar = (Button) findViewById(R.id.btnSomar);
+
+        btnSomar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                double resp = 0;
+
+                String txtN1 = txtNum1.getText().toString();
+                String txtN2 = txtNum2.getText().toString();
+
+                resp = Double.parseDouble(txtN1) + Double.parseDouble(txtN2);
+
+                lblResultado.setText(String.valueOf(resp));
+        }
+        });
+
+    }
+}
